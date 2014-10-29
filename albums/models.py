41fbba2bd3989b3e71ee6db1c8871cf9fa51a930
Marcos Guerrini <marcosguerrini@gmail.com>
2014-10-29 19:23:28 -0300

    Trabajo en Tracks, Albums y Artists

--- albums/models.py
+++ albums/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-from artist.models import Artist
+from artists.models import Artist
 
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
     cover = models.FileField(upload_to="albums")
     artist = models.ForeignKey(Artist)
+
+    def __unicode__(self):
+        return self.title
--- artists/models.py
+++ artists/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Artist(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, blank=True)
-    biography = models.TextField()
+    biography = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.first_name
